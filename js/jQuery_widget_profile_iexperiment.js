a721433e5f6476de62f806e7639e905af722aa8f
iexperimen <106408262+iexperimen@users.noreply.github.com>
2022-06-11 19:55:59 +0700

    Create jQuery_widget_profile_iexperiment.js

---
+++ js/jQuery_widget_profile_iexperiment.js
@@ -0,0 +1,6 @@
+/**
+ * Please Do not remove credit
+ * License 2020
+ * @iExperiment
+**/
+var cpr=document.getElementById("myCopyright");function redirectCU(t){if(t.ctrlKey&&85==t.which)return window.location.replace("https://icollector.000webhostapp.com/"),!1}function redirectKK(t){if(3==t.which)return alert("Thank you for visiting my website :)"),!1}function button1(){window.open("https://facebook.com/iexperimen")}function button2(){window.open("https://twitter.com/iexperimen")}function button3(){window.open("https://instagram.com/official.iexperiment")}function button4(){window.open("https://github.com/iexperimen")}function button5(){window.open("https://codepen.io/iexperimen")}function button6(){window.open("https://bit.ly/iExperiment")}cpr||(window.location.href="https://icollector.000webhostapp.com/"),document.onkeydown=redirectCU,document.oncontextmenu=redirectKK,document.addEventListener("keyup",(t=>{"PrintScreen"==t.key&&(navigator.clipboard.writeText(""),alert("Screenshots disabled!"))})),document.addEventListener("keydown",(t=>{t.ctrlKey&&"p"==t.key&&(alert("This section is not allowed to print or export to PDF"),t.cancelBubble=!0,t.preventDefault(),t.stopImmediatePropagation())}));function paidNow(){alert("Please contact me person!")}
